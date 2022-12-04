a17b34a8e88269cce7fb1cd99e499073d52e1b37
lynnga <lingamliel@gmail.com>
2022-12-04 00:00:25 -0600

    started adding employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,21 @@
+const Employee = require("../lib/Employee");
+
+test("can create a new employee object", () => {
+  const employee = new Employee();
+  expect(typeof employee).toBe("object");
+});
+
+test("can set name in employee object", () => {
+  const employee = new Employee("Lin");
+  expect(employee.name).toBe("Lin");
+});
+
+test("can set employee_id in employee object", () => {
+  const employee = new Employee("Lin", 30);
+  expect(employee.employee_id).toBe(30);
+});
+
+test("can set email in employee object", () => {
+  const employee = new Employee("Lin", 30, "lingamliel@gmail.com");
+  expect(employee.email).toBe("lingamliel@gmail.com");
+});

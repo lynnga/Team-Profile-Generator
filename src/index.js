88995f892821b20e48e0b32b142c147f0add06f6
lynnga <lingamliel@gmail.com>
2022-12-04 02:04:04 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,57 @@
 const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
 
+const team = [];
 function main() {
-  createManager();
+  createManager().then(answers =>{
+    console.log(answers)
+  });
 }
+
 function createManager() {
   console.log("let's start building your team");
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "Who is the manager of this team?",
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "what is the manager's id?",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "what is the manager's email?",
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "what is the manager's office number?",
+    },
+  ]);
+}
+function createTeam() {
   inquirer
-    .promt([
+    .prompt([
       {
-        type: 'input',
-        name: 'name',
-        message: 'Who is the manager of this team?', 
-      },
-      {
-        type: 'input',
-        name: 'managerId',
-        message: 'what is the manager\'s id?',
-      },
-      {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'what is the manager\'s email?',
-      },
-      {
-        type: 'input',
-        name: 'managerOfficeNumber',
-        message: 'what is the manager\'s office number?',
+        type: "list",
+        name: "teamMemeberChoice",
+        message: "which type of team memeber would you like to add?",
+        choices: ["Engineer", "Intern", "I have finished my team"],
       },
     ])
-    .then(answers => {
-      console.log(answers);
-    })
+    .then((answers) => {
+      switch (answer.teamMemberChoice) {
+        case "Engineer":
+          break;
+        case "Intern":
+          break;
+        default:
+      }
+    });
 }
-main()
+function buildTeam() {}
+main();
